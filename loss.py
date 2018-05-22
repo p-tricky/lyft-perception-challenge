0b5b4187b2215e14f29781e87916d1955c81a032
Ubuntu <ubuntu@ip-172-31-1-139.us-west-2.compute.internal>
2018-05-22 18:44:52 +0000

    new loss and model

--- loss.py
+++ loss.py
@@ -34,23 +34,36 @@ class LossBinary:
 
 
 class LossMulti:
-    def __init__(self, jaccard_weight=0, num_classes=1):
-        self.nll_loss = nn.NLLLoss()
+    def __init__(self, jaccard_weight=0, num_classes=1, betas=None, class_weights=None):
+        self.nll_loss = nn.NLLLoss(weight=class_weights)
         self.jaccard_weight = jaccard_weight
         self.num_classes=num_classes
+        self.betas = betas
 
     def __call__(self, outputs, targets):
         loss = self.nll_loss(outputs, targets)
         if self.jaccard_weight:
             cls_weight = self.jaccard_weight / self.num_classes
             eps = 1e-15
-            for cls in range(self.num_classes):
-                jaccard_target = (targets == cls).float()
-                jaccard_output = outputs[:, cls].exp()
-                intersection = (jaccard_output * jaccard_target).sum()
+            
+            # add f1 scores to loss
+            for cls, beta in zip([1,2],[.5, 2]):
+                target = (targets == cls).float()
+                output = outputs[:, cls].exp()
+                correct = (target*output).sum().item()
+                prec = correct / (output.sum().item() + eps)
+                recall = correct / (target.sum().item() + eps)
+                f_score = (1+beta**2)*(prec*recall)/(beta**2*prec+recall)
+#                 import pdb; pdb.set_trace()
+                loss += (1-f_score)/2
+            
+#             for cls in range(self.num_classes):
+#                 jaccard_target = (targets == cls).float()
+#                 jaccard_output = outputs[:, cls].exp()
+#                 intersection = (jaccard_output * jaccard_target).sum()
 
-                union = jaccard_output.sum() + jaccard_target.sum() + eps
-                loss += (1 - intersection / (union - intersection)) * cls_weight
+#                 union = jaccard_output.sum() + jaccard_target.sum() + eps
+#                 loss += (1 - intersection / (union - intersection)) * cls_weight
 
-            loss /= (1 + self.jaccard_weight)
+#             loss /= (1 + self.jaccard_weight)
         return loss
